fc4b37089f12b3089ea0b68aa8cc707829febf76
spencer wilson <spencer@spewil.com>
2024-08-25 12:18:54 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,96 @@
+from threading import Lock
+from flask import Flask, render_template, session, request, \
+    copy_current_request_context
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+user_counts = {}
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count})
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+
+@socketio.event
+def my_event(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socketio.event
+def my_broadcast_event(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socketio.event
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    # for this emit we use a callback function
+    # when the callback function is invoked we know that the message has been
+    # received and it is safe to disconnect
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
+
+
+@socketio.event
+def my_ping():
+    emit('my_pong')
+
+
+# when a client presses a key
+@socketio.event
+def client_keydown(message):
+    user_id = message['data']
+    # get the count and increment, or if it's a new one, set to 0 and increment
+    new_count = user_counts.get(user_id, 0) + 1
+    user_counts.update({user_id: new_count})
+    emit('user_counts', {'data': user_counts})
+
+
+# when a client connects
+@socketio.event
+def connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+
+# when a client refreshes
+@socketio.event
+def refresh_disconnect(message):
+    disconnected_id = message['data']
+    user_counts.pop(disconnected_id)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
---
+++ notes.md
@@ -0,0 +1,2 @@
+"Access to 127.0.0.1 was denied"
+- chrome://net-internals/#sockets -- flush sockets
