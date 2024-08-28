7c785a5b54d37bcec1d7e80532c6bb36e6d870a5
Philip Shamash <philshams@ucla.edu>
2024-08-27 21:07:53 -0700

    display num active users

--- app.py
+++ app.py
@@ -17,22 +17,18 @@ thread_lock = Lock()
 # all events update and/or use this state
 users_inhaling = set()
 users_exhaling = set()
-users_online = set()
 
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-
 # when a client connects
 @socketio.on('connect')
 def on_connect(message):
     user_id = message['id']
-    users_online.add(user_id)
     # send everyone new user list
-    emit('user_connection', list(users_online), broadcast=True)
-
+    emit('update_state', {'inhaling': list(users_inhaling), 'exhaling': list(users_exhaling)}, broadcast=True)
 
 # when a client presses a key
 @socketio.event
@@ -68,10 +64,8 @@ def leave(message):
     user_id = message['data']
     users_inhaling.discard(user_id)
     users_exhaling.discard(user_id)
-    users_online.discard(user_id)
     # send everyone new user list
-    emit('user_connection', list(users_online), broadcast=True)
-    emit('update_state', {'inhaling': list(users_inhaling), 'exhaling': list(users_exhaling)}, roadcast=True)
+    emit('update_state', {'inhaling': list(users_inhaling), 'exhaling': list(users_exhaling)}, broadcast=True)
 
 
 if __name__ == '__main__':
