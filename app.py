0acfd7da03d97c5ea4c16012865a68024db1443a
Philip Shamash <philshams@ucla.edu>
2024-08-29 22:55:36 -0700

    nice buttons

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from threading import Lock
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+import time
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
@@ -18,7 +19,6 @@ thread_lock = Lock()
 users_inhaling = set()
 users_exhaling = set()
 
-
 @app.route('/')
 def breathe():
     return render_template('breathe.html', async_mode=socketio.async_mode)
@@ -29,15 +29,25 @@ def together():
 
 # when a client connects
 @socketio.on('connect')
-def on_connect(message):
-    user_id = message['id']
+def on_connect():
+    emit('assign_user_id', request.sid, broadcast=False)
+    # send everyone new user list
+    emit('update_state', {'inhaling': list(users_inhaling), 'exhaling': list(users_exhaling)}, broadcast=True)
+
+# when a client disconnects
+@socketio.on('disconnect')
+def disconnect():
+    # disconnected_id = message['data']
+    user_id = request.sid
+    users_inhaling.discard(user_id)
+    users_exhaling.discard(user_id)
     # send everyone new user list
     emit('update_state', {'inhaling': list(users_inhaling), 'exhaling': list(users_exhaling)}, broadcast=True)
 
 # when a client presses a key
 @socketio.event
-def client_keydown(message):
-    user_id = message['data']
+def client_keydown():
+    user_id = request.sid
     users_inhaling.add(user_id)
     users_exhaling.discard(user_id)
     # send everyone just this keypress update
@@ -45,8 +55,8 @@ def client_keydown(message):
 
 # when a client releases a key
 @socketio.event
-def client_keyup(message):
-    user_id = message['data']
+def client_keyup():
+    user_id = request.sid
     users_inhaling.discard(user_id)
     users_exhaling.add(user_id)
     # send everyone just this keypress update
@@ -54,23 +64,22 @@ def client_keyup(message):
 
 # when a client times out
 @socketio.event
-def timeout(message):
-    user_id = message['data']
+def timeout():
+    user_id = request.sid
     users_inhaling.discard(user_id)
     users_exhaling.discard(user_id)
     # send everyone new user list
     emit('update_state', {'inhaling': list(users_inhaling), 'exhaling': list(users_exhaling)}, broadcast=True)
 
 
-# when a client refreshes
+# when a client refreshes or times out
 @socketio.event
-def leave(message):
-    user_id = message['data']
+def leave():
+    user_id = request.sid
     users_inhaling.discard(user_id)
     users_exhaling.discard(user_id)
     # send everyone new user list
     emit('update_state', {'inhaling': list(users_inhaling), 'exhaling': list(users_exhaling)}, broadcast=True)
-
 
 if __name__ == '__main__':
     socketio.run(app)
